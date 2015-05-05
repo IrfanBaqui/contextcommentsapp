6d45334a5947f0d6bf2966e9c320c55163ff6da0
Derek Sakamoto <dmsakamoto@gmail.com>
2015-05-05 14:21:58 -0700

    Rewrote the comment view and controller to handle multiple comments in a comment box. TODO: Format the comment boxes appropriately.

--- client/app/article/comment/comment.controller.js
+++ client/app/article/comment/comment.controller.js
@@ -4,6 +4,20 @@ console.log('here');
 
 angular.module('contextcommentsApp')
   .controller('CommentBoxCtrl', function ($scope, $http) {
-    $scope.entries = [{name : 'Dave', value: 'a'},{name : 'Dave2', value: 'a2'}
+    $scope.boxes = [
+      {
+        commentedText: 'Comment Box 1', 
+        entries: [
+          {username: 'John', comment: 'Hello, World!', updatedAt: 'Today'}, 
+          {username: 'Jane', comment: 'Hello, John!', updatedAt: 'Today'}
+        ]
+      },
+      {
+        commentedText: 'Comment Box 2', 
+        entries: [
+          {username: 'John', comment: 'Jane you are not the world.', updatedAt: 'Today'}, 
+          {username: 'Jane', comment: 'How do you know?', updatedAt: 'Today'}
+        ]
+      }
     ];
   });
