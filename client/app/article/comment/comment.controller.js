dd4eb890c22047325715c63c0486295aa58081fc
Derek Sakamoto <dmsakamoto@gmail.com>
2015-05-11 12:18:04 -0700

    Got authentication to preliminarily work

--- client/app/article/comment/comment.controller.js
+++ client/app/article/comment/comment.controller.js
@@ -1,9 +1,12 @@
 'use strict';
 
 angular.module('contextcommentsApp')
-  .controller('CommentBoxCtrl', function ($scope, $http, $stateParams, Comment) {
+  .controller('CommentBoxCtrl', function ($scope, $http, $stateParams, Comment, Auth, User) {
     
+    $scope.user = Auth.getCurrentUser();
+
     // set up initial synchronoization for existing comment boxes
+    console.log('User: ', Auth.getCurrentUser(), Auth.isLoggedIn());
 
     // $scope.boxes = Comment.comments;
     $scope.boxes = [{ commentedText: 'Overall comments', entries: [] }];
@@ -15,13 +18,22 @@ angular.module('contextcommentsApp')
     $scope.addCommentEntry = function (index) {
       console.log('Entering addCommentEntry function', $scope.boxes[index].newCommentText);
       // $scope.boxes[index].newCommentText is not pulling the appropriate new comment entries
-
-      if ($scope.boxes[index].newCommentText !== '') {
-        var entry = {username: 'test user', comment: $scope.boxes[index].newCommentText, updatedAt: 'date'};
-        $scope.boxes[index].entries.push(entry);
-        $scope.boxes[index].newCommentText = '';
-        console.log('Object Synced to DB', $scope.boxes);
-        Comment.syncComments($scope.boxes);
+      if (Auth.isLoggedIn()) {
+        if ($scope.boxes[index].newCommentText !== '') {
+          // Auth.getCurrentUser.
+          var entry = {
+            username: $scope.user.name, 
+            pictureId: 'http://graph.facebook.com/' + $scope.user.facebook.id + '/picture', 
+            comment: $scope.boxes[index].newCommentText, 
+            updatedAt: new Date().toString()
+          };
+          $scope.boxes[index].entries.push(entry);
+          $scope.boxes[index].newCommentText = '';
+          console.log('Object Synced to DB', $scope.boxes);
+          Comment.syncComments($scope.boxes);
+        }
+      } else {
+        console.err('You must be logged in to enter a comment');
       }
     }
   });
